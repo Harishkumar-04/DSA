4a119744efe7fb184425dbfa1a048a9511f745a0
HARISHKUMAR M <harishkumarit2004@gmail.com>
2025-12-15 13:43:23 +0530

    Time: 30 ms (79.77%), Space: 59.2 MB (40.47%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -22,7 +22,8 @@ class Solution {
 
 */
 
-//Better Solution 
+
+/*Better Solution 
 
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
@@ -47,43 +48,43 @@ class Solution {
         return new ArrayList<>(result);
     }
 }
+*/
 
-/*Optimal solution
+//Optimal solution
 
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
         Arrays.sort(nums);  
 
-        for (int i = 0; i < nums.length - 2; i++) {
-            if (i > 0 && nums[i] == nums[i - 1])
-                continue; 
+        for(int i=0; i<nums.length-2; i++){
+            if(i>0 && nums[i]==nums[i-1])
+            continue;
 
-            if (nums[i] > 0) break; 
+            if(nums[i]>0)
+            break;
 
-            int left = i + 1;
-            int right = nums.length - 1;
+            int left=i+1;
+            int right=nums.length-1;
 
-            while (left < right) {
-                int sum = nums[i] + nums[left] + nums[right];
+            while(left<right){
+                int sum = nums[i]+nums[left]+nums[right];
 
-                if (sum == 0) {
+                if(sum==0){
                     result.add(Arrays.asList(nums[i], nums[left], nums[right]));
 
-                    while (left < right && nums[left] == nums[left + 1]) left++;
-                    while (left < right && nums[right] == nums[right - 1]) right--;
+                    while(left<right && nums[left]==nums[left+1]) left++;
+                    while(left<right && nums[right]==nums[right-1]) right--;
 
                     left++;
                     right--;
-                } else if (sum < 0) {
-                    left++; 
-                } else {
-                    right--; 
                 }
+                else if(sum<0) left++;
+                else right--;
             }
         }
-
         return result;
     }
 }
-*/
+
+     
