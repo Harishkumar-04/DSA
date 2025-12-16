1f0f7f5493bae4b548a66fba8ae405d7dd5f7c4f
HARISHKUMAR M <harishkumarit2004@gmail.com>
2025-12-16 14:24:23 +0530

    Time: 23 ms (70.67%), Space: 90.5 MB (18.87%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
@@ -1,15 +1,32 @@
+/*Brute Force TC=O(n × k) SC=O(1)
+
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j <= i + k && j < nums.length; j++) {
+                if (nums[i] == nums[j]) {
+                    return true;   
+                }
+            }
+        }
+        return false;  
+    }
+}
+
+*/
+
+//Optimal-HashMap TC=O(n) SC=O(n)
+
 class Solution {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
         Map<Integer, Integer> map = new HashMap<>();
 
         for (int i = 0; i < nums.length; i++) {
             int num = nums[i];
-
             if (map.containsKey(num)) {
                 int val = map.get(num);
-
                 if (i - val <= k) {
-                    return true;
+                    return true;  //if(map.containsKey(nums[i]) && i - map.get(nums[i]) <= k) 
                 }
             }
             map.put(num, i);
