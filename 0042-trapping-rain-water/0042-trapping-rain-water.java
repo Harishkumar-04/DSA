c9f6088e89742b5d27be11e6cff15b89404f06ea
HARISHKUMAR M <harishkumarit2004@gmail.com>
2025-12-17 23:19:26 +0530

    Time: 1 ms (58.31%), Space: 47.6 MB (56.93%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,3 +1,5 @@
+/*Better Solution T=O(n) S=O(n)
+
 class Solution {
     public int trap(int[] height) {
 
@@ -5,22 +7,45 @@ class Solution {
         int[] left = new int[n];
         int[] right = new int[n];
 
-        int max=-1;
-        for(int i=0;i<n;i++){
-            max=Math.max(max,height[i]);
-            left[i]=max;
+        int max = -1;
+        for (int i = 0; i < n; i++) {
+            max = Math.max(max, height[i]);
+            left[i] = max;
         }
 
-        max=-1;
-        for(int i=n-1;i>=0;i--){
-            max=Math.max(max,height[i]);
-            right[i]=max;
+        max = -1;
+        for (int i = n - 1; i >= 0; i--) {
+            max = Math.max(max, height[i]);
+            right[i] = max;
         }
 
-        int total=0;
-        for(int i=0;i<n;i++){
-            total += Math.min(left[i],right[i])-height[i];
+        int total = 0;
+        for (int i = 0; i < n; i++) {
+            total += Math.min(left[i], right[i]) - height[i];
         }
         return total;
+    }
+}
+*/
+
+//Optimal
+
+class Solution {
+    public int trap(int[] height) {
+
+        int left = 0, right = height.length - 1, leftMax = 0, rightMax = 0, water = 0;
+
+        while (left < right) {
+            if (height[left] < height[right]) {
+                leftMax = Math.max(leftMax, height[left]);
+                water += leftMax - height[left];
+                left++;
+            } else {
+                rightMax = Math.max(rightMax, height[right]);
+                water += rightMax - height[right];
+                right--;
+            }
+        }
+        return water;
     }
 }
