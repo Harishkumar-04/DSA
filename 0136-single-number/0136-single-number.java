50dc0b2dcf5b3ee8953312969fd0c00a1789500b
HARISHKUMAR M <harishkumarit2004@gmail.com>
2025-12-18 12:01:12 +0530

    Time: 1 ms (99.89%), Space: 47.1 MB (17.14%) - LeetHub

---
+++ 0136-single-number/0136-single-number.java
@@ -0,0 +1,11 @@
+//T=O(n) S=O(1)
+
+class Solution {
+    public int singleNumber(int[] nums) {
+        int result = 0;
+        for (int num : nums) {
+            result ^= num;
+        }
+        return result;
+    }
+}
