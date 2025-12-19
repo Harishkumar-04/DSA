91b1eff7118685971b41adaf5cafff94df6452e2
HARISHKUMAR M <harishkumarit2004@gmail.com>
2025-12-19 12:29:06 +0530

    Time: 0 ms (100%), Space: 43.7 MB (5.06%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,3 +1,5 @@
+// T=O(n) S=O(1)
+
 class Solution {
     public void sortColors(int[] nums) {
         int low = 0, mid = 0, high = nums.length - 1;
@@ -22,3 +24,26 @@ class Solution {
         nums[j] = temp;
     }
 }
+
+/*
+class Solution {
+    public void sortColors(int[] nums) {
+        int count0 = 0, count1 = 0, count2 = 0;
+        for (int num : nums) {
+            if (num == 0) count0++;
+            else if (num == 1) count1++;
+            else count2++;
+        }
+        int index = 0;
+        for (int i = 0; i < count0; i++) {
+            nums[index++] = 0;
+        }
+        for (int i = 0; i < count1; i++) {
+            nums[index++] = 1;
+        }
+        for (int i = 0; i < count2; i++) {
+            nums[index++] = 2;
+        }
+    }
+}
+*/
