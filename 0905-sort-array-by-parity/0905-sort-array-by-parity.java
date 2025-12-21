fc5f8e37613f73721f078af829cbf7e6278cd957
HARISHKUMAR M <harishkumarit2004@gmail.com>
2025-12-21 23:25:15 +0530

    Time: 1 ms (45.72%), Space: 46.9 MB (21.52%) - LeetHub

--- 0905-sort-array-by-parity/0905-sort-array-by-parity.java
+++ 0905-sort-array-by-parity/0905-sort-array-by-parity.java
@@ -1,20 +1,57 @@
+/*Brute Force
+
 class Solution {
     public int[] sortArrayByParity(int[] nums) {  
-        int[] res = new int[nums.length];  
-        int st=0;
+        int[] result = new int[nums.length];  
+        int i=0;
         for(int num:nums){
             if(num%2==0){
-            res[st]=num;
-            st++;
+            result[i++]=num;
             }
         }
 
         for(int num:nums){
             if(num%2!=0){
-            res[st]=num;
-            st++;
+            result[i++]=num;
             }
         }
-        return res;
+        return result;
+    }
+}
+
+
+class Solution {
+    public int[] sortArrayByParity(int[] nums) {  
+        int[] result = new int[nums.length];  
+        int left=0, right=nums.length-1;
+
+        for(int num:nums){
+            if(num%2==0)
+            result[left++]=num;
+            else
+            result[right--]=num;
+        }
+        return result;
+    }
+}
+*/
+
+class Solution {
+    public int[] sortArrayByParity(int[] nums) {
+        int left = 0, right = nums.length - 1;
+
+        while (left < right) {
+            if (nums[left] % 2 > nums[right]%2) {
+                int temp = nums[left];
+                nums[left] = nums[right];
+                nums[right] = temp;
+            }
+
+            if (nums[left] % 2 == 0)
+                left++;
+            if (nums[right] % 2 == 1)
+                right--;
+        }
+        return nums;
     }
 }
