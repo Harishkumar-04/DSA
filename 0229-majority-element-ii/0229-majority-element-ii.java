a274f4fd23f0da61a2a35571a45ada6d7f65a14e
HARISHKUMAR M <harishkumarit2004@gmail.com>
2025-12-21 20:16:30 +0530

    Time: 2 ms (99.76%), Space: 52.8 MB (15.46%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,4 +1,4 @@
-/*Brute Force
+/* Brute Force T=O(n²) S=O(1)
 
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
@@ -22,23 +22,67 @@ class Solution {
     }
 }
 
-*/
+Better T=O(n) S=O(n)
 
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        int n=nums.length;
+        int n = nums.length;
         List<Integer> result = new ArrayList<>();
-        Map<Integer, Integer> map=new HashMap<>();
+        Map<Integer, Integer> map = new HashMap<>();
 
-        for(int num:nums){
-            map.put(num, map.getOrDefault(num,0)+1);
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
         }
 
-        for(int key:map.keySet()){
-            if(map.get(key)>n/3)
-            result.add(key);
+        for (int key : map.keySet()) {
+            if (map.get(key) > n / 3)
+                result.add(key);
         }
         return result;
     }
 }
 
+*/
+
+//Optimal T=O(n) S=O(1)
+
+class Solution {
+    public List<Integer> majorityElement(int[] nums) {
+        int candidate1 = 0, candidate2 = 1;
+        int count1 = 0, count2 = 0;
+
+        for (int num : nums) {
+            if (num == candidate1)
+                count1++;
+            else if (num == candidate2)
+                count2++;
+            else if (count1 == 0) {
+                candidate1 = num;
+                count1 = 1;
+            } else if (count2 == 0) {
+                candidate2 = num;
+                count2 = 1;
+            } else {
+                count1--;
+                count2--;
+            }
+        }
+
+        count1 = 0;
+        count2 = 0;
+        for (int num : nums) {
+            if (num == candidate1)
+                count1++;
+            else if (num == candidate2)
+                count2++;
+        }
+
+        int n = nums.length;
+        List<Integer> result = new ArrayList<>();
+        if (count1 > n / 3)
+            result.add(candidate1);
+        if (count2 > n / 3)
+            result.add(candidate2);
+        return result;
+    }
+}
