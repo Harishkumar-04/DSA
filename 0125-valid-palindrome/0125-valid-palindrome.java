1bad4f4bee83465946b353db5e76e256458390e9
HARISHKUMAR M <harishkumarit2004@gmail.com>
2025-12-22 21:04:36 +0530

    Time: 3 ms (65.25%), Space: 44.9 MB (43.85%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,28 +1,24 @@
 class Solution {
     public boolean isPalindrome(String s) {
         StringBuilder sb = new StringBuilder();
-
-        // Step 1: build cleaned lowercase string
-        for (int i = 0; i < s.length(); i++) {
-            char ch = s.charAt(i);
-
-            if (Character.isLetterOrDigit(ch)) {
+        for (char ch : s.toCharArray()){
+            if (ch >= 'a' && ch <= 'z'){
+                sb.append(ch);
+            }else if (ch >= 'A' && ch <= 'Z'){
                 sb.append(Character.toLowerCase(ch));
+            } else if (ch >= '0' && ch <= '9') {
+                sb.append(ch);  
             }
         }
-
-        // Step 2: two-pointer check
-        int left = 0;
-        int right = sb.length() - 1;
-
-        while (left < right) {
-            if (sb.charAt(left) != sb.charAt(right)) {
+        int i = 0;
+        int j = sb.length() -1;
+        while (i < j){
+            if(sb.charAt(i) != sb.charAt(j)){
                 return false;
             }
-            left++;
-            right--;
+            i++ ;
+            j-- ;
         }
-
         return true;
     }
 }
