1e8c7676e564fd6f7ffec7792b26087d22666973
HARISHKUMAR M <harishkumarit2004@gmail.com>
2025-12-24 23:52:54 +0530

    Time: 6 ms (43.49%), Space: 44.2 MB (79.1%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,40 +1,19 @@
-/* Brute Force TLE
+
 class Solution {
     public boolean isAnagram(String s, String t) {
         if (s.length() != t.length())
             return false;
 
+        int[] freq = new int[26];
+
         for (int i = 0; i < s.length(); i++) {
-            char ch = s.charAt(i);
-
-            int countS = 0;
-            int countT = 0;
-
-            for (int j = 0; j < s.length(); j++) {
-                if (s.charAt(j) == ch)
-                    countS++;
-                if (t.charAt(j) == ch)
-                    countT++;
-            }
-            if (countS != countT)
+            freq[s.charAt(i) - 'a']++;
+            freq[t.charAt(i) - 'a']--;
+        }
+        for (int check : freq) {
+            if (check != 0)
                 return false;
         }
         return true;
-    }
-}
-*/
-class Solution {
-    public boolean isAnagram(String s, String t) {
-        if (s.length() != t.length())
-            return false;
-
-        char[] a = s.toCharArray();
-        char[] b = t.toCharArray();
-
-        Arrays.sort(a);
-        Arrays.sort(b);
-
-        return Arrays.equals(a, b);
-        
     }
 }
