43e8334758810abbe34357716737c87416fdc9c1
HARISHKUMAR M <harishkumarit2004@gmail.com>
2025-12-25 11:51:36 +0530

    Time: 6 ms (84.49%), Space: 47.1 MB (17.69%) - LeetHub

---
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
@@ -0,0 +1,37 @@
+/* Brute Force TLE
+
+class Solution {
+    public int firstUniqChar(String s) {
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            int count = 0;
+
+            for (int j = 0; j < s.length(); j++) {
+                if (s.charAt(j) == ch)
+                    count++;
+            }
+            if (count == 1)
+                return i;
+        }
+        return -1;
+    }
+}
+*/
+
+class Solution {
+    public int firstUniqChar(String s) {
+
+        int[] freq=new int[26];
+
+        for(int i=0;i<s.length();i++){
+            freq[s.charAt(i)-'a']++;
+        }
+
+        for(int i=0;i<s.length();i++){
+            if(freq[s.charAt(i)-'a']==1)
+            return i;
+        }
+        return -1;
+    }
+}
