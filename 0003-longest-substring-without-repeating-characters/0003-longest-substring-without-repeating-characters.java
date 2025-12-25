991b3892a1a2934a58329f207e7c1c92cf053ecf
HARISHKUMAR M <harishkumarit2004@gmail.com>
2025-12-25 22:44:01 +0530

    Time: 6 ms (67.09%), Space: 46.6 MB (23.36%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,29 +1,4 @@
-/* Brute Force T=O(n²) S=O(1)
 
-class Solution {
-    public int lengthOfLongestSubstring(String s) {
-        int max=0;
-
-        for(int i=0;i<s.length();i++){
-            boolean[] seen=new boolean[256];
-            //int count=0;
-
-            for(int j=i;j<s.length();j++){
-                char c=s.charAt(j);
-
-                if(seen[c])
-                break;
-
-                seen[c]=true;    
-                //count++; 
-                max=Math.max(max,j-i+1);
-            }
-            //max=Math.max(max,count);
-        }
-        return max;
-    }
-}
-*/
 // Optimal T=O(n) S=O(1)
 
 class Solution {
@@ -45,23 +20,3 @@ class Solution {
         return max;
     }
 }
-/*
-class Solution {
-    public int lengthOfLongestSubstring(String s) {
-        int[] last = new int[256];
-        Arrays.fill(last, -1);
-        int left = 0, max = 0;
-
-        for (int right = 0; right < s.length(); right++) {
-            char c = s.charAt(right);
-
-            if (last[c] >= left)
-                left = last[c] + 1;
-
-            last[c] = right;
-            max = Math.max(max, right - left + 1);
-        }
-        return max;
-    }
-}
-*/
