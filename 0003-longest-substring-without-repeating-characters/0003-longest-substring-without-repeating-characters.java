4b9b7cb8cee72dc542e4128275e04b57ce224a40
HARISHKUMAR M <harishkumarit2004@gmail.com>
2025-12-25 22:23:40 +0530

    Time: 6 ms (67.09%), Space: 46.4 MB (31.62%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -23,7 +23,7 @@ class Solution {
         return max;
     }
 }
-
+*/
 class Solution {
     public int lengthOfLongestSubstring(String s) {
         Set<Character> set = new HashSet<>();
@@ -43,23 +43,23 @@ class Solution {
         return max;
     }
 }
-*/
 
-class Solution {
-    public int lengthOfLongestSubstring(String s) {
-        int[] last=new int[256];
-        Arrays.fill(last,-1);
-        int left=0, max=0;
 
-        for(int right=0;right<s.length();right++){
-            char c=s.charAt(right);
+// class Solution {
+//     public int lengthOfLongestSubstring(String s) {
+//         int[] last=new int[256];
+//         Arrays.fill(last,-1);
+//         int left=0, max=0;
 
-            while(last[c]>=left)
-            left=last[c]+1;
+//         for(int right=0;right<s.length();right++){
+//             char c=s.charAt(right);
 
-            last[c]=right;
-            max=Math.max(max,right-left+1);
-        }
-        return max;
-    }
-}
+//             while(last[c]>=left)
+//             left=last[c]+1;
+
+//             last[c]=right;
+//             max=Math.max(max,right-left+1);
+//         }
+//         return max;
+//     }
+// }
