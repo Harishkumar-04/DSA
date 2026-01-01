6018239f4e2e485ec23bc0f344f07da7cb15a33f
HARISHKUMAR M <harishkumarit2004@gmail.com>
2026-01-01 23:02:28 +0530

    Time: 4 ms (14.46%), Space: 42.8 MB (97.64%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -1,6 +1,6 @@
 class Solution {
     public String longestCommonPrefix(String[] strs) {
-        if (strs == null || strs.length == 0) return "";
+        //if (strs == null || strs.length == 0) return "";
         String match="";
 
             for(int i=0;i<strs[0].length();i++){
@@ -15,26 +15,3 @@ class Solution {
         return match; 
     }
 }
-// class Solution {
-//     public String longestCommonPrefix(String[] strs) {
-//         if(strs.length== 1) return strs[0];
-//         StringBuilder match = new StringBuilder();
-
-//             for(int i=0;i<strs[0].length();i++){
-//                 char ch = strs[0].charAt(i);
-//                 boolean flag = false;
-//                 for(int j=1;j<strs.length;j++){
-//                     if(i<strs[j].length() && ch == strs[j].charAt(i)){
-//                         flag = true;
-//                         continue;
-//                     }
-//                     else flag = false;
-                        
-//                 }
-                
-//                 if(flag) match.append(ch); 
-//             }
-        
-//         return match.toString(); 
-//     }
-// }
