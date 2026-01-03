03885b1569945770d4cae878e280a987582af70b
HARISHKUMAR M <harishkumarit2004@gmail.com>
2026-01-03 11:34:58 +0530

    Time: 0 ms (100%), Space: 47.4 MB (51.05%) - LeetHub

--- 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.java
+++ 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.java
@@ -1,20 +1,20 @@
-class Solution {
-    public int repeatedNTimes(int[] nums) {
+// class Solution {
+//     public int repeatedNTimes(int[] nums) {
 
-        for (int i = 0; i < nums.length; i++) {
-            int count = 0;
+//         for (int i = 0; i < nums.length; i++) {
+//             int count = 0;
 
-            for (int j = 0; j < nums.length; j++) {
-                if (nums[i] == nums[j])
-                    count++;
-            }
-            if (count > 1) {
-                return nums[i];
-            }
-        }
-        return -1;
-    }
-}
+//             for (int j = 0; j < nums.length; j++) {
+//                 if (nums[i] == nums[j])
+//                     count++;
+//             }
+//             if (count > 1) {
+//                 return nums[i];
+//             }
+//         }
+//         return -1;
+//     }
+// }
 
 
 // class Solution {
@@ -26,7 +26,7 @@ class Solution {
 //         for (int num : nums) {
 //             map.put(num, map.getOrDefault(num, 0) + 1);
 
-//             if (map.get(num) == n)
+//             if (map.get(num) > 1)
 //                 return num;
 //         }
 //         return -1;
@@ -44,3 +44,14 @@ class Solution {
 //         return -1;
 //     }
 // }
+
+class Solution {
+    public int repeatedNTimes(int[] nums) {
+
+        for(int i=0;i<nums.length-2;i++){
+            if(nums[i]==nums[i+1] || nums[i]==nums[i+2])
+            return nums[i];
+        }
+        return nums[nums.length-1];
+    }
+}
