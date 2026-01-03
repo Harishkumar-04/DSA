8653ed4d511c4f4c0e1b20f86e1da0d30ee59d03
HARISHKUMAR M <harishkumarit2004@gmail.com>
2026-01-03 11:15:35 +0530

    Time: 2 ms (54.18%), Space: 47.6 MB (25.52%) - LeetHub

--- 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.java
+++ 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.java
@@ -1,18 +1,21 @@
-// class Solution {
-//     public int repeatedNTimes(int[] nums) {
-//         int n=nums.length/2;
-//         int[] freq=new int[nums.length];
+class Solution {
+    public int repeatedNTimes(int[] nums) {
+        int n = nums.length;
 
-//         for(int i=0;i<nums.length;i++){
-//             freq[nums[i]-1]++;
-//         }
-//         for(int i=0;i<freq.length;i++){
-//             if(freq[i]==n)
-//             return i+1;
-//         }
-//         return -1;
-//     }
-// }
+        for (int i = 0; i < n; i++) {
+            int count = 0;
+
+            for (int j = 0; j < n; j++) {
+                if (nums[i] == nums[j])
+                    count++;
+            }
+            if (count > 1) {
+                return nums[i];
+            }
+        }
+        return -1;
+    }
+}
 
 // class Solution {
 //     public int repeatedNTimes(int[] nums) {
@@ -30,14 +33,14 @@
 //     }
 // }
 
-class Solution {
-    public int repeatedNTimes(int[] nums) {
-        Set<Integer> set = new HashSet<>();
+// class Solution {
+//     public int repeatedNTimes(int[] nums) {
+//         Set<Integer> set = new HashSet<>();
 
-        for (int num : nums) {
-            if (!set.add(num))
-                return num;
-        }
-        return -1;
-    }
-}
+//         for (int num : nums) {
+//             if (!set.add(num))
+//                 return num;
+//         }
+//         return -1;
+//     }
+// }
