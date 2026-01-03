d7d1d4e23920bfbf693c412381f443cf7347148e
HARISHKUMAR M <harishkumarit2004@gmail.com>
2026-01-03 23:29:34 +0530

    Time: 3 ms (87.53%), Space: 43.4 MB (25.77%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,16 +1,41 @@
+// class Solution {
+//     public boolean isValid(String s) {
+//         Stack<Character> stack=new Stack<>();
+
+//         for(char ch:s.toCharArray()){
+//             if(ch=='(') stack.push(')');
+//             else if(ch=='[') stack.push(']');
+//             else if(ch=='{') stack.push('}');
+//             else{
+//                 if(stack.isEmpty() || stack.pop()!=ch)
+//                 return false;
+//             }
+//         }
+//         return stack.isEmpty(); 
+//     }
+// }
+
 class Solution {
     public boolean isValid(String s) {
         Stack<Character> stack=new Stack<>();
 
         for(char ch:s.toCharArray()){
-            if(ch=='(') stack.push(')');
-            else if(ch=='[') stack.push(']');
-            else if(ch=='{') stack.push('}');
+            if(ch=='(' || ch=='[' || ch=='{'){
+                stack.push(ch);
+            }
             else{
-                if(stack.isEmpty() || stack.pop()!=ch)
+                if(stack.isEmpty())
                 return false;
+                
+                char top=stack.pop();
+
+                if(ch==')' && top!='(') return false;
+                if(ch==']' && top!='[') return false;
+                if(ch=='}' && top!='{') return false;
             }
         }
-        return stack.isEmpty(); 
+        return stack.isEmpty();
     }
 }
+
+            
