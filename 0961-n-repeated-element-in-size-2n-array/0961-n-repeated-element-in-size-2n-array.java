37e9c1207f452f6ba437e4b6a70d3f64fef640e0
HARISHKUMAR M <harishkumarit2004@gmail.com>
2026-01-03 11:25:04 +0530

    Time: 2 ms (54.18%), Space: 47.8 MB (6.18%) - LeetHub

--- 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.java
+++ 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.java
@@ -1,37 +1,37 @@
-// class Solution {
-//     public int repeatedNTimes(int[] nums) {
-
-//         for (int i = 0; i < nums.length; i++) {
-//             int count = 0;
-
-//             for (int j = 0; j < nums.length; j++) {
-//                 if (nums[i] == nums[j])
-//                     count++;
-//             }
-//             if (count == nums.length / 2) {
-//                 return nums[i];
-//             }
-//         }
-//         return -1;
-//     }
-// }
-
-
 class Solution {
     public int repeatedNTimes(int[] nums) {
-        int n = nums.length / 2;
 
-        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int count = 0;
 
-        for (int num : nums) {
-            map.put(num, map.getOrDefault(num, 0) + 1);
-
-            if (map.get(num) == n)
-                return num;
+            for (int j = 0; j < nums.length; j++) {
+                if (nums[i] == nums[j])
+                    count++;
+            }
+            if (count > 1) {
+                return nums[i];
+            }
         }
         return -1;
     }
 }
+
+
+// class Solution {
+//     public int repeatedNTimes(int[] nums) {
+//         int n = nums.length / 2;
+
+//         Map<Integer, Integer> map = new HashMap<>();
+
+//         for (int num : nums) {
+//             map.put(num, map.getOrDefault(num, 0) + 1);
+
+//             if (map.get(num) == n)
+//                 return num;
+//         }
+//         return -1;
+//     }
+// }
 
 // class Solution {
 //     public int repeatedNTimes(int[] nums) {
