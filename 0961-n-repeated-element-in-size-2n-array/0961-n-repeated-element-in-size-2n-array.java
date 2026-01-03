1250e2bb31dc6c758ce6fd5daf0629dec5cea945
HARISHKUMAR M <harishkumarit2004@gmail.com>
2026-01-03 11:47:39 +0530

    Time: 0 ms (100%), Space: 47.8 MB (12.72%) - LeetHub

--- 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.java
+++ 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.java
@@ -1,49 +1,56 @@
-// class Solution {
-//     public int repeatedNTimes(int[] nums) {
+/* Brute T=O(n²) S=O(1)
 
-//         for (int i = 0; i < nums.length; i++) {
-//             int count = 0;
+class Solution {
+    public int repeatedNTimes(int[] nums) {
 
-//             for (int j = 0; j < nums.length; j++) {
-//                 if (nums[i] == nums[j])
-//                     count++;
-//             }
-//             if (count > 1) {
-//                 return nums[i];
-//             }
-//         }
-//         return -1;
-//     }
-// }
+        for (int i = 0; i < nums.length; i++) {
+            int count = 0;
 
+            for (int j = 0; j < nums.length; j++) {
+                if (nums[i] == nums[j])
+                    count++;
+            }
+            if (count > 1) {
+                return nums[i];
+            }
+        }
+        return -1;
+    }
+}
 
-// class Solution {
-//     public int repeatedNTimes(int[] nums) {
-//         int n = nums.length / 2;
+// Better T=O(n) S=O(n)
 
-//         Map<Integer, Integer> map = new HashMap<>();
+class Solution {
+    public int repeatedNTimes(int[] nums) {
+        int n = nums.length / 2;
 
-//         for (int num : nums) {
-//             map.put(num, map.getOrDefault(num, 0) + 1);
+        Map<Integer, Integer> map = new HashMap<>();
 
-//             if (map.get(num) > 1)
-//                 return num;
-//         }
-//         return -1;
-//     }
-// }
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
 
-// class Solution {
-//     public int repeatedNTimes(int[] nums) {
-//         Set<Integer> set = new HashSet<>();
+            if (map.get(num) > 1)
+                return num;
+        }
+        return -1;
+    }
+}
 
-//         for (int num : nums) {
-//             if (!set.add(num))
-//                 return num;
-//         }
-//         return -1;
-//     }
-// }
+class Solution {
+    public int repeatedNTimes(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+
+        for (int num : nums) {
+            if (!set.add(num))
+                return num;
+        }
+        return -1;
+    }
+}
+
+*/
+
+// Optimal - (distance ≤ 2 trick) T=O(n) S=O(1)
 
 class Solution {
     public int repeatedNTimes(int[] nums) {
@@ -51,6 +58,9 @@ class Solution {
         for(int i=0;i<nums.length-2;i++){
             if(nums[i]==nums[i+1] || nums[i]==nums[i+2])
             return nums[i];
+            /*Core trick: since one element fills half the array, 
+            two of its occurrences must appear either next to each other or
+            with just one element in between.*/
         }
         return nums[nums.length-1];
     }
