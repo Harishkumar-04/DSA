0f4ff8cdaa052c754053fcd3c5dfd6bac6482b9f
HARISHKUMAR M <harishkumarit2004@gmail.com>
2026-01-03 23:33:37 +0530

    Time: 3 ms (87.53%), Space: 43.3 MB (56.56%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -17,29 +17,4 @@ class Solution {
         return stack.isEmpty();
     }
 }
-/*
-class Solution {
-    public boolean isValid(String s) {
-        Stack<Character> stack = new Stack<>();
 
-        for (char ch : s.toCharArray()) {
-            if (ch == '(' || ch == '[' || ch == '{') {
-                stack.push(ch);
-            } else {
-                if (stack.isEmpty())
-                    return false;
-
-                char top = stack.pop();
-
-                if (ch == ')' && top != '(')
-                    return false;
-                if (ch == ']' && top != '[')
-                    return false;
-                if (ch == '}' && top != '{')
-                    return false;
-            }
-        }
-        return stack.isEmpty();
-    }
-}
-*/
