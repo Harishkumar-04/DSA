6aec23c144509cac34d96abe354896f5f7eb3998
HARISHKUMAR M <harishkumarit2004@gmail.com>
2026-01-07 01:35:57 +0530

    Time: 0 ms (100%), Space: 42.7 MB (65.87%) - LeetHub

--- 1455-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/1455-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.java
+++ 1455-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/1455-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.java
@@ -1,22 +1,38 @@
+/* T=O(n × m) S=O(n)
+
 class Solution {
     public int isPrefixOfWord(String sentence, String searchWord) {
-        String[] word = sentence.split(" ");
+        String[] words = sentence.split(" ");
 
-        for (int i = 0; i < word.length; i++) {
+        for (int i = 0; i < words.length; i++) {
 
-            if (word[i].length() < searchWord.length())
-                continue;
+            if (words[i].length() < searchWord.length()) continue;
 
-            int j;
-            for (j = 0; j < searchWord.length(); j++) {
-                if (word[i].charAt(j) != searchWord.charAt(j)) {
+            boolean isPrefix = true;
+            for (int j = 0; j < searchWord.length(); j++) {
+                if (words[i].charAt(j) != searchWord.charAt(j)) {
+                    isPrefix = false;
                     break;
                 }
             }
-
-            if (j == searchWord.length())
-                return i + 1;
+            if (isPrefix) return i + 1;
         }
         return -1;
     }
 }
+*/
+// T=O(n × m) S=O(n)
+
+class Solution {
+    public int isPrefixOfWord(String sentence, String searchWord) {
+        String[] words = sentence.split(" ");
+
+        for (int i = 0; i < words.length; i++) {
+            if(words[i].length()<searchWord.length()) continue;
+            if(words[i].startsWith(searchWord))
+            return i+1;
+        }
+        return -1;
+    }
+}
+
