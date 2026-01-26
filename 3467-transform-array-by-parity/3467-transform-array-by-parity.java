99deb79fab92fbbc2bb31bad2723a2e1c65e2054
HARISHKUMAR M <harishkumarit2004@gmail.com>
2026-01-26 23:15:27 +0530

    Time: 1 ms (99.71%), Space: 46.9 MB (67%) - LeetHub

--- 3467-transform-array-by-parity/3467-transform-array-by-parity.java
+++ 3467-transform-array-by-parity/3467-transform-array-by-parity.java
@@ -1,5 +1,23 @@
 class Solution {
     public int[] transformArray(int[] nums) {
+        int count = 0;
+        for (int num : nums)
+            if (num % 2 == 0)
+                count++;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (count != 0) {
+                nums[i] = 0;
+                count--;
+            } else
+                nums[i] = 1;
+        }
+        return nums;
+    }
+}
+
+/*class Solution {
+    public int[] transformArray(int[] nums) {
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] % 2 == 0)
                 nums[i] = 0;
@@ -10,3 +28,4 @@ class Solution {
         return nums;
     }
 }
+*/
