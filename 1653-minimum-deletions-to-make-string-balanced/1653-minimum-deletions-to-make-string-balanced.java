a0f8894c90b0d38b71f683152b881510a8946362
HARISHKUMAR M <harishkumarit2004@gmail.com>
2026-02-07 23:34:19 +0530

    Time: 18 ms (100%), Space: 48.1 MB (9.21%) - LeetHub

---
+++ 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int minimumDeletions(String s) {
+        int countb=0,delete=0;
+
+        for(char ch:s.toCharArray()){
+            if(ch=='b')
+            countb++;
+            else
+            delete=Math.min(delete+1,countb);
+        }
+        return delete;
+    }
+}
