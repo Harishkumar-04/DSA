7c23de0ac1e7f30eeead7cf6187727509c3968be
HARISHKUMAR M <harishkumarit2004@gmail.com>
2026-02-19 11:46:51 +0530

    Time: 30 ms (37.38%), Space: 47.6 MB (44.03%) - LeetHub

--- 1451-rearrange-words-in-a-sentence/1451-rearrange-words-in-a-sentence.java
+++ 1451-rearrange-words-in-a-sentence/1451-rearrange-words-in-a-sentence.java
@@ -1,5 +1,21 @@
 class Solution {
     public String arrangeWords(String text) {
+        
+        text = text.toLowerCase();
+        
+        String[] words = text.split(" ");
+        
+        Arrays.sort(words, (a, b) -> a.length() - b.length());
+        
+        String result = String.join(" ", words);
+        
+        return Character.toUpperCase(result.charAt(0)) + result.substring(1);
+    }
+}
+
+
+/*class Solution {
+    public String arrangeWords(String text) {
         String str=text.toLowerCase();
         String[] arr=str.split(" ");
 
@@ -7,7 +23,6 @@ class Solution {
 
         StringBuilder sb=new StringBuilder();  
         arr[0]=arr[0].substring(0,1).toUpperCase()+arr[0].substring(1,arr[0].length());
-       // System.out.println(arr[0]);      
         
         for(String s:arr)
         sb.append(s).append(" ");
@@ -16,3 +31,4 @@ class Solution {
         return sb.toString().trim();
     }
 }
+*/
