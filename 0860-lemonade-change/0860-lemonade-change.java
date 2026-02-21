be5cb95dab0eac64796fd7455b7301dccad2221f
HARISHKUMAR M <harishkumarit2004@gmail.com>
2026-02-21 23:27:53 +0530

    Time: 2 ms (99.15%), Space: 72.5 MB (68.39%) - LeetHub

--- 0860-lemonade-change/0860-lemonade-change.java
+++ 0860-lemonade-change/0860-lemonade-change.java
@@ -1,26 +1,29 @@
 class Solution {
     public boolean lemonadeChange(int[] bills) {
         int c5 = 0, c10 = 0;
-        boolean b = true;
+
         for (int num : bills) {
             if (num == 5) {
                 c5++;
             }
 
-            else if (num == 10 && c5 >= 1) {
+            else if (num == 10) {
+                if(c5==0) return false;
                 c10++;
                 c5--;
-            } else if (num == 20) {
-                if (c5 >= 1 && c10 >= 1) {
+            }
+
+            else {
+                if (c5 > 0 && c10 > 0) {
                     c5--;
                     c10--;
+                } else if (c5 >= 3) {
+                    c5 -= 3;
+                } else {
+                    return false;
                 }
-                else if(c5>=3)
-                c5-=3;
-                else b=false;
-            } else
-                b = false;
+            }
         }
-        return b;
+        return true;
     }
 }
