07a54e6dc1b6ecb8b43bbf6096719af47d407f87
HARISHKUMAR M <harishkumarit2004@gmail.com>
2026-02-24 20:47:27 +0530

    Time: 3 ms (9.09%), Space: 44.6 MB (11.62%) - LeetHub

--- 2645-minimum-additions-to-make-valid-string/2645-minimum-additions-to-make-valid-string.java
+++ 2645-minimum-additions-to-make-valid-string/2645-minimum-additions-to-make-valid-string.java
@@ -1,60 +1,37 @@
 class Solution {
     public int addMinimum(String word) {
-        Stack<Character> stack=new Stack<>();
-        int count=0;
-        for(char ch:word.toCharArray()){
-            if(stack.isEmpty()){
-                if(ch=='b'){
+        Stack<Character> stack = new Stack<>();
+        int count = 0;
+
+        for (char ch : word.toCharArray()) {
+
+            if (!stack.isEmpty()) {
+                char prev = stack.peek();
+
+                if (prev == 'a' && ch == 'c')
                     count++;
-                    stack.push(ch);
-                }
-                else if(ch=='a')
-                stack.push(ch);
-                else{
-                    count+=2;
-                    stack.push(ch);
-                }
+                else if (prev == 'b' && ch == 'a')
+                    count++;
+                else if (prev == 'c' && ch == 'b')
+                    count++;
+                else if (prev == ch)
+                    count += 2;
+            } else {
+                if (ch == 'b')
+                    count++;
+                else if (ch == 'c')
+                    count += 2;
             }
-            else{
-                if(ch=='a' && stack.peek()=='a'){
-                count+=2;
-                stack.push(ch);
-                }
-                else if(ch=='b' && stack.peek()=='a'){
-                stack.push(ch);
-                }
-                else if(ch=='c' && stack.peek()=='a'){
-                count++;
-                stack.push(ch);
-                }
-                else if(ch=='a' && stack.peek()=='b'){
-                count++;
-                stack.push(ch);
-                }
-                else if(ch=='b' && stack.peek()=='b'){
-                count+=2;
-                stack.push(ch);
-                }
-                else if(ch=='c' && stack.peek()=='b')
-                stack.push(ch);
-                else{
-                    if(ch=='a' && stack.peek()=='c')
-                    stack.push(ch);
-                    else if(ch=='b' && stack.peek()=='c'){
-                        count++;
-                        stack.push(ch);
-                    }
-                    else{
-                        count+=2;
-                        stack.push(ch);
-                    }
-                }
-            }
+
+            stack.push(ch);
         }
-        if(stack.peek()=='b')
-        count++;
-        if(stack.peek()=='a')
-        count+=2;
-        return count;       
+
+        char last = stack.peek();
+        if (last == 'a')
+            count += 2;
+        else if (last == 'b')
+            count += 1;
+
+        return count;
     }
 }
