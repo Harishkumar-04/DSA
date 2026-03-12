a76fb42defd1b959c7d27e439a1cd5b94672bb47
HARISHKUMAR M <harishkumarit2004@gmail.com>
2026-03-12 19:01:53 +0530

    Time: 103 ms (5.26%), Space: 48.2 MB (5.42%) - LeetHub

---
+++ 0506-relative-ranks/0506-relative-ranks.java
@@ -0,0 +1,51 @@
+class Solution {
+    public String[] findRelativeRanks(int[] score) {
+        int n = score.length;
+        int[] copy = Arrays.copyOf(score, n);
+        Arrays.sort(score);
+
+        int gold = 0, silver = 0, bronze = 0;
+        String[] arr = new String[n];
+
+        if(n<2){
+            arr[0]="Gold Medal";
+            return arr;
+        }
+
+        else if (n >= 3) {
+            gold = score[n - 1];
+            silver = score[n - 2];
+            bronze = score[n - 3];
+
+            for (int i = 0; i < n; i++) {
+                if (copy[i] == gold) {
+                    arr[i] = "Gold Medal";
+                } else if (copy[i] == silver) {
+                    arr[i] = "Silver Medal";
+                } else if (copy[i] == bronze) {
+                    arr[i] = "Bronze Medal";
+                } else {
+                    for (int j = 0; j < n - 3; j++) {
+                        if (score[j] == copy[i]) {
+                            arr[i] = String.valueOf(n - j);
+                        }
+                    }
+                }
+            }
+            return arr;
+        } else {
+            gold = score[n - 1];
+            silver = score[n - 2];
+
+            for (int i = 0; i < n; i++) {
+                if (copy[i] == gold) {
+                    arr[i] = "Gold Medal";
+                } else if (copy[i] == silver) {
+                    arr[i] = "Silver Medal";
+                }
+            }
+            return arr;
+        }
+
+    }
+}
