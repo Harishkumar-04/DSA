53f2a0f4435404739d23aae11baad8c48b8a3fd3
HARISHKUMAR M <harishkumarit2004@gmail.com>
2026-04-01 01:51:32 +0530

    Time: 1499 ms (6.58%), Space: 200 MB (82.24%) - LeetHub

--- 3527-find-the-most-common-response/3527-find-the-most-common-response.java
+++ 3527-find-the-most-common-response/3527-find-the-most-common-response.java
@@ -1,25 +1,27 @@
 class Solution {
     public String findCommonResponse(List<List<String>> responses) {
+        Set<String> set=new HashSet<>();
+        Map<String,Integer> map=new TreeMap<>();
 
-        Map<String, Integer> map = new HashMap<>();
+        for(int i=0;i<responses.size();i++){
+            set.addAll(responses.get(i));
 
-        int max = 0;
+            for(String s:set){
+                map.put(s,map.getOrDefault(s,0)+1);
+            }
+            set.clear();
+        }
+
+        int max=Integer.MIN_VALUE;
         String key = "";
-
-        for (int i = 0; i < responses.size(); i++) {
-            Set<String> set = new HashSet<>(responses.get(i));
-
-            for (String s : set) {
-                int count = map.getOrDefault(s, 0) + 1;
-                map.put(s, count);
-
-                if (count > max || (count == max && s.compareTo(key) < 0)) {
-                    max = count;
-                    key = s;
-                }
+        
+        for(Map.Entry<String,Integer> entry:map.entrySet()){
+            if(entry.getValue()>max){
+                max=entry.getValue();
+                key=entry.getKey();
             }
         }
         return key;
-
+        
     }
 }
