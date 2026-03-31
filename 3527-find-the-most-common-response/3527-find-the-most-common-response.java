fb5d89be3f5d38de6e31102c1839780d4304c537
HARISHKUMAR M <harishkumarit2004@gmail.com>
2026-04-01 01:52:38 +0530

    Time: 110 ms (97.7%), Space: 200.2 MB (77.96%) - LeetHub

--- 3527-find-the-most-common-response/3527-find-the-most-common-response.java
+++ 3527-find-the-most-common-response/3527-find-the-most-common-response.java
@@ -1,3 +1,4 @@
+/*
 class Solution {
     public String findCommonResponse(List<List<String>> responses) {
         Set<String> set=new HashSet<>();
@@ -23,5 +24,33 @@ class Solution {
         }
         return key;
         
+    }
+}
+*/
+
+class Solution {
+    public String findCommonResponse(List<List<String>> responses) {
+
+        Map<String, Integer> map = new HashMap<>();
+
+        int max = 0;
+        String key = "";
+
+        for (int i = 0; i < responses.size(); i++) {
+            Set<String> set = new HashSet<>(responses.get(i));
+
+            for (String s : set) {
+                int count = map.getOrDefault(s, 0) + 1;
+                map.put(s, count);
+
+
+                if (count > max || (count == max && s.compareTo(key) < 0)) {
+                    max = count;
+                    key = s;
+                }
+            }
+        }
+        return key;
+
     }
 }
