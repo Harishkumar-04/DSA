48b66c9b14cb0a534a88db77dbbdc31e2adcd334
HARISHKUMAR M <harishkumarit2004@gmail.com>
2026-04-03 13:46:27 +0530

    Time: 1332 ms (37.06%), Space: 44 MB (85.44%) - LeetHub

--- 1395-count-number-of-teams/1395-count-number-of-teams.java
+++ 1395-count-number-of-teams/1395-count-number-of-teams.java
@@ -1,19 +1,23 @@
 class Solution {
     public int numTeams(int[] rating) {
-        int n=rating.length;
-        int count=0;
+        int n = rating.length;
+        int count = 0;
 
-        for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
-                int valid=0;
-                for(int k=j+1;k<n;k++){
-                    if(rating[i]<rating[j] && rating[j]<rating[k])
-                    valid++;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
 
-                    if(rating[i]>rating[j] && rating[j]>rating[k])
-                    valid++;
+                if (rating[i] < rating[j]) {
+                    for (int k = j + 1; k < n; k++) {
+                        if (rating[k] > rating[j]) 
+                        count++;
+                    }
+                } 
+                else if (rating[i] > rating[j]) {
+                    for (int k = j + 1; k < n; k++) {
+                        if (rating[k] < rating[j]) 
+                        count++;
+                    }
                 }
-                count+=valid;
             }
         }
         return count;
