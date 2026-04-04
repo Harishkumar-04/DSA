e2736591c729a2bda12b34d595fba638c29cb0e8
HARISHKUMAR M <harishkumarit2004@gmail.com>
2026-04-04 23:10:12 +0530

    Time: 3 ms (52.48%), Space: 44.8 MB (45.57%) - LeetHub

--- 3823-reverse-letters-then-special-characters-in-a-string/3823-reverse-letters-then-special-characters-in-a-string.java
+++ 3823-reverse-letters-then-special-characters-in-a-string/3823-reverse-letters-then-special-characters-in-a-string.java
@@ -1,29 +1,33 @@
 class Solution {
     public String reverseByType(String s) {
-        List<Character> list1=new ArrayList<>();
-        List<Character> list2=new ArrayList<>();
 
-        for(char ch:s.toCharArray()){
-            if(Character.isLowerCase(ch))
-            list1.add(ch);
-            else
-            list2.add(ch);
-        }
-        Collections.reverse(list1);
-        Collections.reverse(list2);
+        StringBuilder lower = new StringBuilder();
+        StringBuilder symbol = new StringBuilder();
 
-        StringBuilder sb=new StringBuilder();
-        int i=0,j=0;
-        for(char ch:s.toCharArray()){
-            if(Character.isLowerCase(ch)){
-            sb.append(list1.get(i));
-            i++;
-            }
-            else{
-            sb.append(list2.get(j));
-            j++;
+        for (char ch : s.toCharArray()) {
+            if (Character.isLowerCase(ch)) {
+                lower.append(ch);
+            } else {
+                symbol.append(ch);
             }
         }
-        return sb.toString();
+
+        lower.reverse();
+        symbol.reverse();
+
+        StringBuilder result = new StringBuilder();
+        int i = 0, j = 0;
+
+        for (char ch : s.toCharArray()) {
+            if (Character.isLowerCase(ch)) {
+                result.append(lower.charAt(i));
+                i++;
+            } else {
+                result.append(symbol.charAt(j));
+                j++;
+            }
+        }
+
+        return result.toString();
     }
 }
