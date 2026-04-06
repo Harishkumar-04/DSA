8adc513da675414d021817e2bdf72bd5c1da6939
HARISHKUMAR M <harishkumarit2004@gmail.com>
2026-04-06 20:44:30 +0530

    Time: 7 ms (22.53%), Space: 46.6 MB (31.32%) - LeetHub

--- 0423-reconstruct-original-digits-from-english/0423-reconstruct-original-digits-from-english.java
+++ 0423-reconstruct-original-digits-from-english/0423-reconstruct-original-digits-from-english.java
@@ -1,94 +1,93 @@
 class Solution {
     public String originalDigits(String s) {
-
         int[] freq = new int[26];
 
-        for (char ch : s.toCharArray()) {
-            freq[ch - 'a']++;
+        for(char ch:s.toCharArray()){
+            freq[ch-'a']++;
         }
 
-        StringBuilder sb = new StringBuilder();
+        StringBuilder sb=new StringBuilder();
 
-        while (freq['z' - 'a'] > 0) {
+        while(freq['z'-'a']>0){
             sb.append('0');
-            freq['z' - 'a']--;
-            freq['e' - 'a']--;
-            freq['r' - 'a']--;
-            freq['o' - 'a']--;
+            freq['z'-'a']--;
+            freq['e'-'a']--;
+            freq['r'-'a']--;
+            freq['o'-'a']--;
         }
 
-        while (freq['w' - 'a'] > 0) {
+        while(freq['w'-'a']>0){
             sb.append('2');
-            freq['t' - 'a']--;
-            freq['w' - 'a']--;
-            freq['o' - 'a']--;
+            freq['t'-'a']--;
+            freq['w'-'a']--;
+            freq['o'-'a']--;
         }
 
-        while (freq['u' - 'a'] > 0) {
+        while(freq['u'-'a']>0){
             sb.append('4');
-            freq['f' - 'a']--;
-            freq['o' - 'a']--;
-            freq['u' - 'a']--;
-            freq['r' - 'a']--;
+            freq['f'-'a']--;
+            freq['o'-'a']--;
+            freq['u'-'a']--;
+            freq['r'-'a']--;
         }
 
-        while (freq['x' - 'a'] > 0) {
+        while(freq['x'-'a']>0){
             sb.append('6');
-            freq['s' - 'a']--;
-            freq['i' - 'a']--;
-            freq['x' - 'a']--;
+            freq['s'-'a']--;
+            freq['i'-'a']--;
+            freq['x'-'a']--;
         }
 
-        while (freq['g' - 'a'] > 0) {
+        while(freq['g'-'a']>0){
             sb.append('8');
-            freq['e' - 'a']--;
-            freq['i' - 'a']--;
-            freq['g' - 'a']--;
-            freq['h' - 'a']--;
-            freq['t' - 'a']--;
+            freq['e'-'a']--;
+            freq['i'-'a']--;
+            freq['g'-'a']--;
+            freq['h'-'a']--;
+            freq['t'-'a']--;
         }
 
-        while (freq['h' - 'a'] > 0) {
+        while(freq['h'-'a']>0){
             sb.append('3');
-            freq['t' - 'a']--;
-            freq['h' - 'a']--;
-            freq['r' - 'a']--;
-            freq['e' - 'a'] -= 2;
+            freq['t'-'a']--;
+            freq['h'-'a']--;
+            freq['r'-'a']--;
+            freq['e'-'a']-=2;
         }
-
-        while (freq['f' - 'a'] > 0) {
+        
+        while(freq['f'-'a']>0){
             sb.append('5');
-            freq['f' - 'a']--;
-            freq['i' - 'a']--;
-            freq['v' - 'a']--;
-            freq['e' - 'a']--;
+            freq['f'-'a']--;
+            freq['i'-'a']--;
+            freq['v'-'a']--;
+            freq['e'-'a']--;
         }
 
-        while (freq['s' - 'a'] > 0) {
+        while(freq['s'-'a']>0){
             sb.append('7');
-            freq['s' - 'a']--;
-            freq['e' - 'a'] -= 2;
-            freq['v' - 'a']--;
-            freq['n' - 'a']--;
+            freq['s'-'a']--;
+            freq['e'-'a']-=2;
+            freq['v'-'a']--;
+            freq['n'-'a']--;
         }
 
-        while (freq['o' - 'a'] > 0) {
+        while(freq['o'-'a']>0){
             sb.append('1');
-            freq['o' - 'a']--;
-            freq['n' - 'a']--;
-            freq['e' - 'a']--;
+            freq['o'-'a']--;
+            freq['n'-'a']--;
+            freq['e'-'a']--;
         }
 
-        while (freq['i' - 'a'] > 0) {
+        while(freq['i'-'a']>0){
             sb.append('9');
-            freq['n' - 'a'] -= 2;
-            freq['i' - 'a']--;
-            freq['e' - 'a']--;
+            freq['n'-'a']-=2;
+            freq['i'-'a']--;
+            freq['e'-'a']--;
         }
 
-        char[] arr = sb.toString().toCharArray();
+        char[] arr=sb.toString().toCharArray();
         Arrays.sort(arr);
-
+        
         return new String(arr);
     }
 }
