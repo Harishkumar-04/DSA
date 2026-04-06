e39ac2cf1ebe067ebde138ac80615ac8b07afe8a
HARISHKUMAR M <harishkumarit2004@gmail.com>
2026-04-07 00:09:39 +0530

    Time: 85 ms (39.62%), Space: 167.5 MB (51.29%) - LeetHub

--- 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.java
+++ 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.java
@@ -3,26 +3,17 @@ class Solution {
         List<List<Integer>> result = new ArrayList<>();
         List<Integer> win = new ArrayList<>();
         List<Integer> loss = new ArrayList<>();
-        Map<Integer, Integer> map1 = new HashMap<>();
-        Map<Integer, Integer> map2 = new HashMap<>();
+        Map<Integer, Integer> map = new HashMap<>();
 
         for (int[] x : matches) {
-            map1.put(x[0], map1.getOrDefault(x[0], 0) + 1);
-            
-        }
-        for(int[] x:matches){
-            if(map1.containsKey(x[1])){
-            map1.remove(x[1]);
-            }
-            map2.put(x[1], map2.getOrDefault(x[1], 0) + 1);
+            map.putIfAbsent(x[0], 0);
+            map.put(x[1], map.getOrDefault(x[1], 0) + 1);
         }
 
-        for (int num : map1.keySet()) {
-            win.add(num);
-        }
-
-        for (int num : map2.keySet()) {
-            if(map2.get(num)==1){
+        for (int num : map.keySet()) {
+            if (map.get(num) == 0) {
+                win.add(num);
+            } else if (map.get(num) == 1) {
                 loss.add(num);
             }
         }
