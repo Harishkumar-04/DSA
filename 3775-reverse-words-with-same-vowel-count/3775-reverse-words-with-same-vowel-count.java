b0552c0af4e35dcd603b9e8abeedd435287993f9
HARISHKUMAR M <harishkumarit2004@gmail.com>
2026-04-08 11:04:58 +0530

    Time: 54 ms (87.34%), Space: 62.1 MB (82.4%) - LeetHub

--- 3775-reverse-words-with-same-vowel-count/3775-reverse-words-with-same-vowel-count.java
+++ 3775-reverse-words-with-same-vowel-count/3775-reverse-words-with-same-vowel-count.java
@@ -1,55 +1,31 @@
 class Solution {
     public String reverseWords(String s) {
-        int count = 0;
-        StringBuilder sb = new StringBuilder();
-        for (String str : s.split(" ")) {
-            for (char c : str.toCharArray()) {
-                if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
-                    count++;
-                }
-            }
-            sb.append(str).append(" ");
-            break;
-        }
-
-        List<String> list = new ArrayList<>();
-        for (String str : s.split(" ")) {
-            list.add(str);
-        }
-        if(list.size()>1){
+        String[] words=s.split(" ");
+        StringBuilder sb=new StringBuilder();
         
+        int first=countVowels(words[0]);
+        sb.append(words[0]).append(" ");
 
-        for (int i = 1; i < list.size() - 1; i++) {
-            String str2 = list.get(i);
-            int count2 = 0;
-            for (char c : str2.toCharArray()) {
-                if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
-                    count2++;
-                }
+        for(int i=1;i<words.length;i++){
+            int curr=countVowels(words[i]);
+            if(curr==first){
+                sb.append(new StringBuilder(words[i]).reverse());
             }
-            if (count == count2) {
-                String rev = new StringBuilder(str2).reverse().toString();
-                sb.append(rev).append(" ");
-            } else {
-                sb.append(str2).append(" ");
+            else{
+                sb.append(words[i]);
             }
-        }
-
-            int count3 = 0;
-            String str3 = list.get(list.size() - 1);
-            for (char c : str3.toCharArray()) {
-                if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
-                    count3++;
-                }
-            }
-            if (count == count3) {
-                String rev2 = new StringBuilder(str3).reverse().toString();
-                sb.append(rev2);
-            } else {
-                sb.append(str3);
-            }
+            sb.append(" ");
         }
         return sb.toString().trim();
+    }
 
+    private int countVowels(String str){
+        int count=0;
+        for (char c : str.toCharArray()) {
+            if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
+                count++;
+            }
+        }
+        return count;
     }
 }
