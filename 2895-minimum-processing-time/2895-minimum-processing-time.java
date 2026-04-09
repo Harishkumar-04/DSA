aa5861c1ebad211a3d98604e261e0201da0c4f25
HARISHKUMAR M <harishkumarit2004@gmail.com>
2026-04-09 11:40:07 +0530

    Time: 77 ms (84.97%), Space: 87.1 MB (28.32%) - LeetHub

--- 2895-minimum-processing-time/2895-minimum-processing-time.java
+++ 2895-minimum-processing-time/2895-minimum-processing-time.java
@@ -3,19 +3,12 @@ class Solution {
         Collections.sort(processorTime);
         Collections.sort(tasks);
 
-        int i=tasks.size()-1,max1=0,max2=0;
+        int i=tasks.size()-1,max=0;
         for(int num:processorTime){
-            for(;i>=0;i--){
-                max1=Math.max(max1,num+tasks.get(i));
-                if((i%4)==0) break;
-            }
-            i--;
-            max2=Math.max(max2,max1);
+            max=Math.max(max,num+tasks.get(i));
+
+            i-=4;
         } 
-        return max2;     
+        return max;     
     }
 }
-
-// 30 68 112 132 138 166 232 300
-// 0  1   2   3   4   5   6  7
-
