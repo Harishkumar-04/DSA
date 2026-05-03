a0be2259ed1abaef4582ddd8a7fa301ffbad0c93
HARISHKUMAR M <harishkumarit2004@gmail.com>
2026-05-03 13:37:27 +0530

    Time: 5 ms (35.91%), Space: 42.7 MB (69.27%) - LeetHub

--- 3918-sum-of-primes-between-number-and-its-reverse/3918-sum-of-primes-between-number-and-its-reverse.java
+++ 3918-sum-of-primes-between-number-and-its-reverse/3918-sum-of-primes-between-number-and-its-reverse.java
@@ -21,10 +21,15 @@ class Solution {
 
         for(int i=start;i<=end;i++){
             int count=0;
-            for(int j=1;j<=i;j++){
-                if(i%j==0){
-                    count++;
-                    if(count>2) break;
+            for(int j=1;j*j<=i;j++){
+                if (i % j == 0) {
+                    if (j * j == i) {
+                        count++; 
+                    } else {
+                        count += 2;
+                    }
+
+                    if (count > 2) break;
                 }
             }
             if(count==2){
