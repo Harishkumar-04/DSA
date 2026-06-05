e5e73d13de6458bcc87539ca86888dffcc49efa6
HARISHKUMAR M <harishkumarit2004@gmail.com>
2026-06-06 01:03:38 +0530

    Time: 521 ms (10.42%), Space: 0B (100%) - LeetHub

--- 0619-biggest-single-number/0619-biggest-single-number.sql
+++ 0619-biggest-single-number/0619-biggest-single-number.sql
@@ -5,5 +5,17 @@ select(
     having count(num)=1 
     order by num desc 
     limit 1
-    ) 
+    )
     as num;
+
+/*
+
+select max(num) as num from mynumbers 
+where num in(
+    select num 
+    from mynumbers 
+    group by num 
+    having count(*)=1
+    );
+
+*/
