905396c44f873b0a1473b84089bf6e4846e377db
HARISHKUMAR M <harishkumarit2004@gmail.com>
2026-06-14 16:54:53 +0530

    Time: 4 ms (100%), Space: 225.5 MB (75.85%) - LeetHub

--- 2181-merge-nodes-in-between-zeros/2181-merge-nodes-in-between-zeros.java
+++ 2181-merge-nodes-in-between-zeros/2181-merge-nodes-in-between-zeros.java
@@ -8,29 +8,52 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+
+ /*
 class Solution {
     public ListNode mergeNodes(ListNode head) {
 
-        ListNode temp=head.next;
-        ListNode prev=null;
-        ListNode newHead=null;
-        int sum=0;
-        while(temp!=null){
-            sum+=temp.val;
-            if(temp.val==0){
-                ListNode n=new ListNode(sum);
-                if(newHead==null){
-                    newHead=n;
-                    prev=newHead;
+        ListNode temp = head.next;
+        ListNode prev = null;
+        ListNode newHead = null;
+        int sum = 0;
+        while (temp != null) {
+            if (temp.val != 0) {
+                sum += temp.val;
+            } else {
+                ListNode n = new ListNode(sum);
+                if (newHead == null) {
+                    newHead = n;
+                    prev = newHead;
+                } else {
+                    prev.next = n;
+                    prev = n;
                 }
-                else{
-                    prev.next=n;
-                    prev=n;
-                } 
-                sum=0;    
+                sum = 0;
             }
-            temp=temp.next;             
+            temp = temp.next;
         }
         return newHead;
+    }
+}
+*/
+
+class Solution {
+    public ListNode mergeNodes(ListNode head) {
+        ListNode modify=head.next;
+        ListNode temp=modify;
+
+        while(temp!=null){
+            int sum=0;
+            while(temp.val!=0){
+                sum+=temp.val;
+                temp=temp.next;
+            }
+            modify.val=sum;
+            temp=temp.next;
+            modify.next=temp;
+            modify=modify.next;   
+        }
+        return head.next;
     }
 }
