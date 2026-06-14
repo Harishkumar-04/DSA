e156344807851199cba467d36cf61cd2a06f155a
HARISHKUMAR M <harishkumarit2004@gmail.com>
2026-06-14 18:39:23 +0530

    Time: 1 ms (100%), Space: 49.7 MB (13.19%) - LeetHub

--- 1669-merge-in-between-linked-lists/1669-merge-in-between-linked-lists.java
+++ 1669-merge-in-between-linked-lists/1669-merge-in-between-linked-lists.java
@@ -8,35 +8,63 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+
+ /*
+
 class Solution {
     public ListNode mergeInBetween(ListNode list1, int a, int b, ListNode list2) {
         ListNode temp1=list1;
-        ListNode ath=null;
-        ListNode bth=null;
+        ListNode beforeA=null;
+        ListNode afterB=null;
         int cnt=0;
 
         while(temp1!=null){
 
             if(cnt==a-1){
-                ath=temp1;
+                beforeA=temp1;
             }
 
             if(cnt==b){
-                bth=temp1.next;
+                afterA=temp1.next;
                 break;
             }
             
             temp1=temp1.next;
             cnt++;
         }
-        ath.next=list2;
+        beforeA.next=list2;
         ListNode temp2=list2;
 
         while(temp2.next!=null){
             temp2=temp2.next;
         }
 
-        temp2.next=bth;
+        temp2.next=afterB;
+
+        return list1;
+    }
+}
+
+*/
+
+class Solution {
+    public ListNode mergeInBetween(ListNode list1, int a, int b, ListNode list2) {
+        ListNode left=list1;
+        ListNode right=list1;
+        
+        for(int i=1;i<a;i++){
+            left=left.next;
+        }
+        for(int i=1;i<=b;i++){
+            right=right.next;
+        }
+
+        left.next=list2;
+        ListNode temp=list2;
+        while(temp.next!=null){
+            temp=temp.next;
+        }
+        temp.next=right.next;
 
         return list1;
     }
