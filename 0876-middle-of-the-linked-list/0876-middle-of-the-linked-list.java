890aad6ebc5a9b51809e70902ce3dc510f309ac4
HARISHKUMAR M <harishkumarit2004@gmail.com>
2026-06-19 00:58:31 +0530

    Time: 0 ms (100%), Space: 40.8 MB (99.99%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.java
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.java
@@ -8,6 +8,9 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+
+ /*
+
 class Solution {
     public ListNode middleNode(ListNode head) {
         ListNode temp=head;
@@ -27,3 +30,19 @@ class Solution {
         return temp;
     }
 }
+
+*/
+
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        ListNode fast=head;
+        ListNode slow=head;
+
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        return slow;
+    }
+}
+
