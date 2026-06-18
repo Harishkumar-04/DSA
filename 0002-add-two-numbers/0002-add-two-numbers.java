f02b45f7bab94ad13b0e753e584419267605648a
HARISHKUMAR M <harishkumarit2004@gmail.com>
2026-06-18 16:20:30 +0530

    Time: 1 ms (100%), Space: 46.5 MB (37.05%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.java
+++ 0002-add-two-numbers/0002-add-two-numbers.java
@@ -10,27 +10,31 @@
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode dummy=new ListNode(0);
-        ListNode curr=dummy;
-        ListNode temp1=l1;
-        ListNode temp2=l2;
-        int carry=0;
+        ListNode dummy = new ListNode(0);
+        ListNode curr = dummy;
+        ListNode temp1 = l1;
+        ListNode temp2 = l2;
+        int carry = 0;
 
-        while(temp1!=null || temp2!=null){
-            int sum=carry;
-            if(temp1!=null) sum+=temp1.val;
-            if(temp2!=null) sum+=temp2.val;
-            ListNode newNode=new ListNode(sum%10);
-            carry=sum/10;
-
-            curr.next=newNode;
-            curr=curr.next;
-
-            if(temp1!=null) temp1=temp1.next;
-            if(temp2!=null) temp2=temp2.next;
-
-            if(carry!=0) curr.next=new ListNode(carry);
+        while (temp1 != null || temp2 != null) {
+            int sum = carry;
+            
+            if (temp1 != null) {
+                sum += temp1.val;
+                temp1 = temp1.next;
+            }
+            if (temp2 != null) {
+                sum += temp2.val;
+                temp2 = temp2.next;
+            }
+            ListNode newNode = new ListNode(sum % 10);
+            carry = sum / 10;
+            curr.next = newNode;
+            curr = curr.next;
         }
-        return dummy.next;      
+        if (carry != 0)
+            curr.next = new ListNode(carry);
+
+        return dummy.next;
     }
 }
