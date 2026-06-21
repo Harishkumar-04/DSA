047238dc6352c341920fc78e7082a47b579d0ddc
HARISHKUMAR M <harishkumarit2004@gmail.com>
2026-06-21 23:17:59 +0530

    Time: 85 ms (7.28%), Space: 46.6 MB (78.07%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -8,6 +8,8 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+
+ /*
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
         ArrayList<Integer> ans=new ArrayList<>();
@@ -30,5 +32,40 @@ class Solution {
             temp=temp.next;
         }
         return head;
+    }
+}
+*/
+
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+
+        if(lists.length==0) return null;
+        
+        ListNode head=lists[0];
+        
+        for(int i=1;i<lists.length;i++){
+            head=merge(head,lists[i]);
+        }
+        return head;
+    }
+
+
+    ListNode merge(ListNode list1, ListNode list2){
+        ListNode dummy=new ListNode(0);
+        ListNode res=dummy;
+        while(list1!=null && list2!=null){
+            if(list1.val<=list2.val){
+                res.next=list1;
+                list1=list1.next;
+            }else{
+                res.next=list2;
+                list2=list2.next;
+            }
+            res=res.next;
+        }
+        if(list1==null) res.next=list2;
+        else res.next=list1;
+
+        return dummy.next;
     }
 }
