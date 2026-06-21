ade2e0c3ffe8c282d00aabc56e1ce814daa61772
HARISHKUMAR M <harishkumarit2004@gmail.com>
2026-06-21 10:11:34 +0530

    Time: 52 ms (5.15%), Space: 93.6 MB (5.04%) - LeetHub

--- 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.java
+++ 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.java
@@ -1,5 +1,27 @@
 class Solution {
     public int maxIceCream(int[] costs, int coins) {
-        
-    }
+       Queue<Integer> q=new PriorityQueue<>();
+       int c=0;
+       for(int i:costs) q.add(i);
+       while(!q.isEmpty() && q.peek()<=coins){
+          c++;
+          coins-=q.poll();
+       }
+       return c;
+
+        // Arrays.sort(costs);
+
+        // int cnt=0, ice=0;
+
+        // for(int num:costs){
+        //    if(num<=coins){
+        //     cnt++;
+        //     coins-=num;
+        //    }else{
+        //     break;
+        //    }
+        // }
+        // return cnt;
+    } 
+      
 }
