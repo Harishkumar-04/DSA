f4ec08f038bd23216bf9250f93f4dd0fd328de1e
HARISHKUMAR M <harishkumarit2004@gmail.com>
2026-07-02 19:00:09 +0530

    Time: 54 ms (66.7%), Space: 68.3 MB (90.27%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.java
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.java
@@ -21,6 +21,7 @@ class Solution {
 }
 */
 
+/*
 class Solution {
     public int totalFruit(int[] fruits) {
         int n = fruits.length;
@@ -41,6 +42,32 @@ class Solution {
                     l++;
                 }
             }
+            maxlen=Math.max(maxlen,r-l+1);
+            r++;
+        }
+        return maxlen;
+    }
+}
+*/
+
+class Solution {
+    public int totalFruit(int[] fruits) {
+        int n = fruits.length;
+        int maxlen=0;
+        int l=0,r=0;maxlen=0;
+        HashMap<Integer,Integer> map=new HashMap<>();
+
+        while(r<fruits.length){
+            map.put(fruits[r],map.getOrDefault(fruits[r],0)+1);
+
+            if(map.size()>2){
+                map.put(fruits[l],map.get(fruits[l])-1);
+                if(map.get(fruits[l])==0){
+                    map.remove(fruits[l]);
+                }
+                l++;
+            }
+
             maxlen=Math.max(maxlen,r-l+1);
             r++;
         }
