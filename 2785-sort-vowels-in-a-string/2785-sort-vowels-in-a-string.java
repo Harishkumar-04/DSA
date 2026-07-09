3f590cfe547e82390d285f88156c548105a78e44
HARISHKUMAR M <harishkumarit2004@gmail.com>
2026-07-09 10:53:56 +0530

    Time: 77 ms (17.95%), Space: 51.4 MB (6.61%) - LeetHub

--- 2785-sort-vowels-in-a-string/2785-sort-vowels-in-a-string.java
+++ 2785-sort-vowels-in-a-string/2785-sort-vowels-in-a-string.java
@@ -1,24 +1,27 @@
 class Solution {
     public String sortVowels(String s) {
-        StringBuilder sb=new StringBuilder();
+        ArrayList<Character> list=new ArrayList<>();
         for(char ch:s.toCharArray()){
-            if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'||ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U')
-            sb.append(ch);
-        }
-        char[] str=sb.toString().toCharArray();
-        Arrays.sort(str);
-
-        StringBuilder sb2=new StringBuilder();
-        int i=0;
-
-        for(char ch:s.toCharArray()){
-            if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'||ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U'){
-            sb2.append(str[i]);
-            i++;
+            if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u' || ch=='A' || ch=='E' ||ch=='I' || ch=='O' || ch=='U'){
+                list.add(ch);
             }
-            else
-            sb2.append(ch);
         }
-        return sb2.toString();
+        Collections.sort(list);
+        System.out.print(list);
+
+        StringBuilder sb=new StringBuilder();
+        int j=0;
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+            if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u' || ch=='A' || ch=='E' ||ch=='I' || ch=='O' || ch=='U'){
+                sb.append(list.get(j));
+                j++;
+            }
+            else{
+                sb.append(ch);
+            }
+        }
+        return sb.toString();
+
     }
 }
