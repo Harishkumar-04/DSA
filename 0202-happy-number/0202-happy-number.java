fff45ae87a63d2fb20912c91555471c03c8a707a
HARISHKUMAR M <harishkumarit2004@gmail.com>
2026-07-22 19:35:13 +0530

    Time: 2 ms (15.32%), Space: 42.6 MB (21.83%) - LeetHub

--- 0202-happy-number/0202-happy-number.java
+++ 0202-happy-number/0202-happy-number.java
@@ -1,3 +1,4 @@
+/*
 class Solution {
     public boolean isHappy(int n) {
 
@@ -15,5 +16,27 @@ class Solution {
             num/=10;
         }
         return sum;
+    }
+}
+*/
+
+class Solution {
+    public boolean isHappy(int n) {
+        HashSet<Integer> set=new HashSet<>();
+
+        while(n!=1){
+            if(set.contains(n)){
+                return false;
+            }
+            set.add(n);
+            int sum=0;
+            while(n>0){
+                int rem=n%10;
+                sum+=rem*rem;
+                n/=10;
+            }
+            n=sum;
+        }
+        return true;
     }
 }
