068e8ec670890a1421ce87098c8402d8a58bcc67
HARISHKUMAR M <harishkumarit2004@gmail.com>
2026-08-16 23:24:24 +0530

    Time: 164 ms (0%), Space: 47.1 MB (0%) - LeetHub

--- 4021-minimum-operations-to-make-a-rotated-palindrome-i/4021-minimum-operations-to-make-a-rotated-palindrome-i.java
+++ 4021-minimum-operations-to-make-a-rotated-palindrome-i/4021-minimum-operations-to-make-a-rotated-palindrome-i.java
@@ -3,19 +3,16 @@ class Solution {
 
         int ans=Integer.MAX_VALUE;
         for(int k=0;k<s.length() && k<ans;k++){
-            //String str=s.substring(k)+s.substring(0,k);
-            int i=0,j=s.length()-1,op=k;
+            String str=s.substring(k)+s.substring(0,k);
+            int i=0,j=str.length()-1,op=k;
             while(i<j){
-                int x=s.charAt(i)-'a';
-                int y=s.charAt(j)-'a';
-
-                int d=Math.abs(x-y);
-                op+=Math.min(d,26-d); //op+=Math.min((x-y+26)%26,(y-x+26)%26);
+                int x=str.charAt(i)-'a';
+                int y=str.charAt(j)-'a';
+                op+=Math.min((x-y+26)%26,(y-x+26)%26);
                 i++;
                 j--;
             }
             ans=Math.min(ans,op);
-            s=s.substring(1)+s.charAt(0);
         }
         return ans;
     }
