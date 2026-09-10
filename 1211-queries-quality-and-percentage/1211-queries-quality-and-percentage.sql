53ba024296829bdfb939f455d389cee138e0ab8e
HARISHKUMAR M <harishkumarit2004@gmail.com>
2026-09-10 10:01:18 +0530

    Time: 2120 ms (5.01%), Space: 0B (100%) - LeetHub

--- 1211-queries-quality-and-percentage/1211-queries-quality-and-percentage.sql
+++ 1211-queries-quality-and-percentage/1211-queries-quality-and-percentage.sql
@@ -6,11 +6,22 @@ SELECT query_name,
                 WHEN rating < 3 THEN 1
                 ELSE 0
             END
-        ) * 100 / COUNT(*),
+        ) * 100 / COUNT(query_name),
         2
     ) AS poor_query_percentage
 FROM Queries
 GROUP BY query_name;
 
--- ROUND(AVG(IF(rating < 3, 100, 0)), 2) AS poor_query_percentage
+-- Round(Sum(rating/position)/count(*),2)
+-- round(sum(case when rating < 3 then 1 else 0 end) * 100 / count(*), 2)
+-- ROUND(SUM(rating < 3) * 100 / COUNT(*),2)
+
+-- round((sum(rating/position))/count(query_name),2)
+-- round(sum(case when rating < 3 then 1 else 0 end) * 100 / count(query_name), 2)
+
+-- ROUND(AVG(rating / position), 2)
+-- ROUND(AVG(IF(rating < 3, 100, 0)), 2)
+-- ROUND(AVG(rating < 3) * 100, 2)
+
+
 
