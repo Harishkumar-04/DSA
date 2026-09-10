6faa68c82ea05a637454465ec884d69952da8a0c
HARISHKUMAR M <harishkumarit2004@gmail.com>
2026-09-11 00:21:11 +0530

    Time: 964 ms (6.56%), Space: 0B (100%) - LeetHub

--- 1193-monthly-transactions-i/1193-monthly-transactions-i.sql
+++ 1193-monthly-transactions-i/1193-monthly-transactions-i.sql
@@ -1,2 +1,8 @@
-select date_format(trans_date,'%Y-%m') as month, country, count(trans_date) as trans_count, sum(if(state='approved',1,0)) as  approved_count, sum(amount) as trans_total_amount, sum(if(state='approved',amount,0)) as approved_total_amount from transactions
-group by month, country order by month;
+select date_format(trans_date,'%Y-%m') as month, country, 
+count(trans_date) as trans_count, 
+sum(if(state='approved',1,0)) as  approved_count, 
+sum(amount) as trans_total_amount, 
+sum(if(state='approved',amount,0)) as approved_total_amount 
+from transactions
+group by month, country 
+order by month;
