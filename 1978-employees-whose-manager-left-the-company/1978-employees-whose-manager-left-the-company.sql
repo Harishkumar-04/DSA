10335e5a99d4249ba634613ddab7b1200649ed75
HARISHKUMAR M <harishkumarit2004@gmail.com>
2026-09-10 16:57:10 +0530

    Time: 397 ms (38.92%), Space: 0B (100%) - LeetHub

--- 1978-employees-whose-manager-left-the-company/1978-employees-whose-manager-left-the-company.sql
+++ 1978-employees-whose-manager-left-the-company/1978-employees-whose-manager-left-the-company.sql
@@ -1,1 +1,16 @@
-SELECT e.employee_id FROM Employees e LEFT JOIN Employees m on e.manager_id=m.employee_id where e.salary<30000 and m.employee_id is null and e.manager_id is not null order by e.employee_id;
+SELECT e.employee_id 
+FROM Employees e 
+LEFT JOIN Employees m 
+on e.manager_id=m.employee_id 
+where e.salary<30000 
+and m.employee_id is null 
+and e.manager_id is not null 
+order by e.employee_id;
+
+/*
+SELECT employee_id
+FROM Employees
+WHERE salary < 30000
+AND manager_id NOT IN (SELECT employee_id FROM Employees)
+ORDER BY employee_id;
+*/
