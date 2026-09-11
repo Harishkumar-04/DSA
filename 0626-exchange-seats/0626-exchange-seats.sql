97bf22e1ce1df9d842c8826b601c99c12b79cf7a
HARISHKUMAR M <harishkumarit2004@gmail.com>
2026-09-11 18:47:33 +0530

    Time: 359 ms (75.31%), Space: 0B (100%) - LeetHub

--- 0626-exchange-seats/0626-exchange-seats.sql
+++ 0626-exchange-seats/0626-exchange-seats.sql
@@ -1,3 +1,15 @@
+SELECT 
+CASE
+    WHEN id=(SELECT MAX(id) FROM Seat) AND id%2=1 THEN id
+    WHEN id%2=1 THEN id+1
+    ELSE id-1
+END AS id, 
+student
+FROM Seat
+ORDER BY id;
+
+
+/*
 SELECT id,
 CASE
     WHEN id=(SELECT MAX(id) FROM Seat) AND id%2=1 THEN student
@@ -5,3 +17,4 @@ CASE
     ELSE LAG(student) OVER (ORDER BY id)
 END AS student
 FROM Seat;
+*/
