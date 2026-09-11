b89eef0318c239ec35510222c72fd74557f8fa15
HARISHKUMAR M <harishkumarit2004@gmail.com>
2026-09-11 19:22:10 +0530

    Time: 531 ms (58.96%), Space: 0B (100%) - LeetHub

--- 1141-user-activity-for-the-past-30-days-i/1141-user-activity-for-the-past-30-days-i.sql
+++ 1141-user-activity-for-the-past-30-days-i/1141-user-activity-for-the-past-30-days-i.sql
@@ -3,3 +3,5 @@ COUNT(DISTINCT user_id) as active_users
 FROM Activity
 WHERE activity_date > '2019-06-27' AND activity_date <= '2019-07-27'
 GROUP BY activity_date;
+
+-- WHERE activity_date BETWEEN '2019-06-28' AND '2019-07-27'
