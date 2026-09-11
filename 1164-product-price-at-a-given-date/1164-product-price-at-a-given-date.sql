ce7987dfb03b3a84ecbdd7fed550ed0b34d40cb9
HARISHKUMAR M <harishkumarit2004@gmail.com>
2026-09-12 01:24:46 +0530

    Time: 1710 ms (5%), Space: 0B (100%) - LeetHub

--- 1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
+++ 1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
@@ -1,13 +1,14 @@
 SELECT product_id,
 IFNULL(
-        (SELECT new_price 
-        FROM Products p2
-        where p1.Product_id=p2.Product_id
-        AND change_date <= '2019-08-16'
-        ORDER BY change_date desc
-        LIMIT 1
-        ),
-    10) AS price 
+        (
+         SELECT new_price 
+         FROM Products p2
+         where p1.Product_id=p2.Product_id
+         AND change_date <= '2019-08-16'
+         ORDER BY change_date desc
+         LIMIT 1
+        ),10
+    ) AS price 
 FROM Products p1
 GROUP BY product_id;
     
