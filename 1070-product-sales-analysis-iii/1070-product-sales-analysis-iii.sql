ade3ca7ee1d0d11fddd834d591261de777b9c6ce
HARISHKUMAR M <harishkumarit2004@gmail.com>
2026-09-13 20:24:29 +0530

    Time: 721 ms (76.91%), Space: 0B (100%) - LeetHub

---
+++ 1070-product-sales-analysis-iii/1070-product-sales-analysis-iii.sql
@@ -0,0 +1,25 @@
+/*
+SELECT s.product_id, s.year as  first_year, s.quantity, s.price 
+FROM Sales s
+JOIN (
+    SELECT product_id, min(year) as first_year
+    FROM Sales
+    GROUP BY product_id
+) AS f
+ON s.product_id=f.product_id
+AND s.first_year=f.first_year;
+*/
+
+SELECT product_id, year as  first_year, quantity, price 
+FROM (
+    SELECT product_id, year, quantity, price,
+    RANK() OVER (
+        PARTITION BY product_id
+        ORDER BY year
+    ) AS f
+    FROM Sales
+) AS s
+WHERE f=1;
+
+
+
