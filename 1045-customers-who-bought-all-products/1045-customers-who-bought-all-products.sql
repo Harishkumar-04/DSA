de61125a3763c5943f6a7a356c65300d53b48aa0
HARISHKUMAR M <harishkumarit2004@gmail.com>
2026-09-13 14:05:58 +0530

    Time: 575 ms (81.82%), Space: 0B (100%) - LeetHub

--- 1045-customers-who-bought-all-products/1045-customers-who-bought-all-products.sql
+++ 1045-customers-who-bought-all-products/1045-customers-who-bought-all-products.sql
@@ -4,6 +4,17 @@ GROUP BY customer_id
 HAVING COUNT(DISTINCT product_key)=(
     SELECT COUNT(product_key)
     FROM Product
-)
-ORDER BY customer_id; 
+);
+
+/*
+SELECT c.customer_id
+FROM Customer c
+JOIN Product p
+    ON c.product_key = p.product_key
+GROUP BY c.customer_id
+HAVING COUNT(DISTINCT c.product_key) = (
+    SELECT COUNT(*)
+    FROM Product
+);
+*/
 
