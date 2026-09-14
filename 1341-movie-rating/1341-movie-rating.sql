e601bfd4a243038971aff859e781de65075668bd
HARISHKUMAR M <harishkumarit2004@gmail.com>
2026-09-14 20:23:03 +0530

    Time: 1246 ms (93.74%), Space: 0B (100%) - LeetHub

---
+++ 1341-movie-rating/1341-movie-rating.sql
@@ -0,0 +1,24 @@
+(
+    SELECT u.name AS results
+    FROM Users u
+    JOIN MovieRating mr
+    ON u.user_id=mr.user_id
+    GROUP BY u.user_id
+    ORDER BY COUNT(mr.movie_id) DESC, u.name
+    LIMIT 1
+)
+UNION ALL
+(
+    SELECT m.title AS results
+    FROM Movies m
+    JOIN MovieRating mr
+    ON m.movie_id=mr.movie_id
+    WHERE MONTH(created_at) = 2
+    AND YEAR(created_at) = 2020
+    GROUP BY m.movie_id
+    ORDER BY AVG(mr.rating) DESC, m.title ASC
+    LIMIT 1
+)
+
+-- WHERE created_at LIKE '2020-02%'
+-- WHERE DATE_FORMAT(created_at, '%Y-%m') = '2020-02'
