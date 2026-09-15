9fd97f5e9d5aecdf97646cf473a1fb9e7774ac57
HARISHKUMAR M <harishkumarit2004@gmail.com>
2026-09-15 13:16:42 +0530

    Time: 692 ms (32.29%), Space: 0B (100%) - LeetHub

--- 0550-game-play-analysis-iv/0550-game-play-analysis-iv.sql
+++ 0550-game-play-analysis-iv/0550-game-play-analysis-iv.sql
@@ -9,5 +9,19 @@ JOIN (
 ON a.player_id=f.player_id
 AND DATEDIFF(a.event_date, f.first_date) = 1;
 
-
+/*
+SELECT ROUND(
+    COUNT(a.player_id) / (SELECT COUNT(DISTINCT player_id) FROM Activity),
+    2
+) AS fraction
+FROM Activity a
+WHERE DATEDIFF(
+    a.event_date,
+    (
+        SELECT MIN(a2.event_date)
+        FROM Activity a2
+        WHERE a2.player_id = a.player_id
+    )
+) = 1;
+*/
    
