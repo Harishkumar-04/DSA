9d2f7b40b13853301b0d280f67cc374d3dc585ba
HARISHKUMAR M <harishkumarit2004@gmail.com>
2026-09-18 12:16:03 +0530

    Time: 2 ms (45.49%), Space: 94.4 MB (70.81%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,31 +1,11 @@
-/* Brute force T=O(n^2) S=O(1)
-
 class Solution {
     public int maxProfit(int[] prices) {
-        int n = prices.length;
-        int maxProfit=0;
+        int minPrice=prices[0],maxProfit=0;
 
-        for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
-                int profit=prices[j]-prices[i];
-                maxProfit=Math.max(maxProfit,profit);
-            }
-        }
-        return maxProfit;
-    }
-}
-        
-*/
-
-// Optimal T=O(n) S=O(1)
-
-class Solution {
-    public int maxProfit(int[] prices) {
-        int maxProfit = 0, minPrice = prices[0];
-        for(int i=1; i<prices.length; i++){           
-            int profit = prices[i] - minPrice;
-            minPrice = Math.min(minPrice, prices[i]);
-            maxProfit = Math.max(maxProfit, profit);
+        for(int i=1;i<prices.length;i++){
+            int profit=prices[i]-minPrice;
+            maxProfit=Math.max(maxProfit,profit);
+            minPrice=Math.min(minPrice,prices[i]);
         }
         return maxProfit;
     }
