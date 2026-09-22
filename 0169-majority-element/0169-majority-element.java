5428479cdfa16b1c460132ab64bb6e6c880d52cf
HARISHKUMAR M <harishkumarit2004@gmail.com>
2026-09-23 01:00:09 +0530

    Time: 2 ms (71.76%), Space: 63.1 MB (5.58%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,52 +1,17 @@
-/*Brute Force T=O(n²) S=O(1)
-
 class Solution {
     public int majorityElement(int[] nums) {
-        int n = nums.length;
-        for (int i = 0; i < n; i++) {
-            int count = 0;
-            for (int j = 0; j < n; j++) {
-                if (nums[i] == nums[j])
-                    count++;
+        int x=0,cnt=0;
+        for(int num:nums){
+            if(cnt==0){
+                x=num;
             }
-            if (count > n / 2)
-                return nums[i];
+            if(x==num){
+                cnt++;
+            }
+            else{
+                cnt--;
+            }
         }
-        return -1;
-    }
-}
-
-Better Solution T=O(n) S=O(n)
-class Solution {
-    public int majorityElement(int[] nums) {
-        int n = nums.length;
-        Map<Integer, Integer> map = new HashMap<>();
-
-        for (int num : nums) {
-            map.put(num, map.getOrDefault(num, 0) + 1);
-
-            if (map.get(num) > n / 2)
-                return num;
-        }
-        return -1;
-    }
-}
-*/
-// Optimal T=O(n) S=O(1)
-
-class Solution {
-    public int majorityElement(int[] nums) {
-        int count = 0, candidate = 0;
-
-        for (int num : nums) {
-            if (count == 0)
-                candidate = num;
-
-            if (num == candidate)
-                count++;
-            else
-                count--;
-        }
-        return candidate;
+        return x;
     }
 }
